8461cfba9225e3db47c674dd468bec87e2947205
Łukasz Niemier <lukasz@niemier.pl>
2022-08-11 13:09:13 +0200

    fix: conditionaly use removed function

--- src/ssh_signature.erl
+++ src/ssh_signature.erl
@@ -1,17 +1,27 @@
 -module(ssh_signature).
 
--compile({nowarn_deprecated_function, {public_key, ssh_encode, 2}}).
--compile({nowarn_deprecated_function, {public_key, ssh_decode, 2}}).
-
 -dialyzer([
     {no_improper_lists, split/1},
-    {no_match, priv_to_public/1}
+    % Needed due to fact that OTP 25 removed `{ed_pri, _, _, _}' key format
+    {no_match, [
+        ed_to_pub/1,
+        pk_sign/3,
+        pk_verify/4,
+        priv_to_public/1,
+        sig_type/2,
+        type_sig/3
+    ]},
+    {nowarn_function, [pk_sign/3, pk_verify/4]}
 ]).
 
 -include_lib("public_key/include/public_key.hrl").
 
 -export([sign/3, sign/4]).
 -export([verify/2]).
+
+-ifdef(TEST).
+-export([priv_to_public/1, decode/1]).
+-endif.
 
 -export_type([namespace/0, hash_algorithm/0]).
 
@@ -39,7 +49,7 @@ sign(Data, Key, NS) -> sign(Data, Key, NS, #{}).
 %%      or `sha512'. Defaults to `sha512'.</li>
 %% </ul>
 %% @end
--spec sign(binary(), public_key:private_key(), namespace(), Opts) ->
+-spec sign(iodata(), public_key:private_key(), namespace(), Opts) ->
     unicode:chardata()
 when
     Opts :: #{
@@ -52,14 +62,16 @@ sign(Data, Key, NS, Opts) ->
         _ -> ok
     end,
     Algo = maps:get(hash, Opts, sha512),
-    R = <<"">>,
-    Signature = public_key:sign(body(Data, NS, R, Algo), Algo, Key),
+    Reserved = <<"">>,
+    Body = body(Data, NS0, Reserved, Algo),
+    Signature = pk_sign(Body, Algo, Key),
     SigType = sig_type(Key, Algo),
     Sig = <<?STRING(SigType), ?STRING(Signature)>>,
     EncPub = encode(priv_to_public(Key)),
     Result =
         <<?MAGIC_PREAMBLE, ?UINT32(?SIG_VERSION), ?STRING(EncPub), ?STRING(NS0),
-            ?STRING(R), ?STRING(atom_to_binary(Algo, utf8)), ?STRING(Sig)>>,
+            ?STRING(Reserved), ?STRING(atom_to_binary(Algo, utf8)),
+            ?STRING(Sig)>>,
     iolist_to_binary([?BEGIN, $\n, split(base64:encode(Result)), $\n, ?END]).
 
 %% @doc Verify `Signature' of `Data'.
@@ -69,7 +81,7 @@ sign(Data, Key, NS, Opts) ->
 %% requirements.
 %%
 %% @end
--spec verify(binary(), unicode:chardata()) -> {ok, Result} | {error, term()} when
+-spec verify(iodata(), unicode:chardata()) -> {ok, Result} | {error, term()} when
     Result :: #{
         ns => namespace(),
         public_key => public_key:public_key(),
@@ -91,17 +103,17 @@ verify(Data, Signature) ->
                     hash_algorithm := Algo
                 }} ->
                     Body = body(Data, NS, R, Algo),
-                    case public_key:verify(Body, Algo, Sig, PublicKey) of
+                    case pk_verify(Body, Algo, Sig, PublicKey) of
                         true ->
                             {ok, #{
-                                public_key => PublicKey,
                                 ns => NS,
+                                public_key => PublicKey,
                                 signature => Sig
                             }};
                         false ->
                             {error, invalid_signature}
                     end;
-                Error ->
+                {error, _} = Error ->
                     Error
             end;
         _ ->
@@ -139,6 +151,20 @@ parse(
 parse(_) ->
     {error, invalid_format}.
 
+pk_sign(Body, _, {ed_pri, _, _, _} = Key) ->
+    public_key:sign(Body, none, Key);
+pk_sign(Body, _, #'ECPrivateKey'{} = Key) ->
+    public_key:sign(Body, none, Key);
+pk_sign(Body, Algo, #'RSAPrivateKey'{} = Key) ->
+    public_key:sign(Body, Algo, Key).
+
+pk_verify(Body, _, Sig, {ed_pub, _, _} = Key) ->
+    public_key:verify(Body, none, Sig, Key);
+pk_verify(Body, _, Sig, {#'ECPoint'{}, _} = Key) ->
+    public_key:verify(Body, none, Sig, Key);
+pk_verify(Body, Algo, Sig, #'RSAPublicKey'{} = Key) ->
+    public_key:verify(Body, Algo, Sig, Key).
+
 body(Data, NS, R, Algo) ->
     H = crypto:hash(Algo, Data),
     <<?MAGIC_PREAMBLE, ?STRING(NS), ?STRING(R),
@@ -156,44 +182,56 @@ sig_type(#'RSAPrivateKey'{}, sha512) ->
     <<"rsa-sha2-512">>.
 
 type_sig(<<"ssh-ed25519">>, {ed_pub, ed25519, _}, _) -> true;
+type_sig(<<"ssh-ed25519">>, {_, {namedCurve, ?'id-Ed25519'}}, _) -> true;
+type_sig(<<"ssh-ed448">>, {_, {namedCurve, ?'id-Ed448'}}, _) -> true;
 type_sig(<<"ssh-ed448">>, {ed_pub, ed448, _}, _) -> true;
 type_sig(<<"rsa-sha2-256">>, #'RSAPublicKey'{}, sha256) -> true;
 type_sig(<<"rsa-sha2-512">>, #'RSAPublicKey'{}, sha512) -> true;
 type_sig(_, _, _) -> false.
 
 -spec priv_to_public(public_key:private_key()) -> public_key:public_key().
-priv_to_public({ed_pri, Type, Pub, _Priv}) ->
-    {ed_pub, Type, Pub};
-priv_to_public(#'ECPrivateKey'{
-    parameters = {namedCurve, Curve},
-    publicKey = PK
-}) ->
-    Type =
-        case Curve of
-            ?'id-Ed25519' -> ed25519;
-            ?'id-Ed448' -> ed448
-        end,
-    {ed_pub, Type, PK};
+priv_to_public({ed_pri, _, _, _} = Priv) ->
+    ed_to_pub(Priv);
+priv_to_public(#'ECPrivateKey'{} = Priv) ->
+    ed_to_pub(Priv);
 priv_to_public(#'RSAPrivateKey'{modulus = Mod, publicExponent = Exp}) ->
     #'RSAPublicKey'{modulus = Mod, publicExponent = Exp};
 priv_to_public(Other) ->
     Other.
 
+-if(?OTP_RELEASE < 25).
+ed_to_pub({ed_pri, Type, Pub, _Priv}) ->
+    {ed_pub, Type, Pub};
+ed_to_pub(#'ECPrivateKey'{
+    parameters = {namedCurve, ?'id-Ed25519'}, publicKey = Pub
+}) ->
+    {ed_pub, ed25519, Pub};
+ed_to_pub(#'ECPrivateKey'{
+    parameters = {namedCurve, ?'id-Ed448'}, publicKey = Pub
+}) ->
+    {ed_pub, ed448, Pub}.
+-else.
+ed_to_pub({ed_pri, Type, Pub, _Priv}) ->
+    {#'ECPoint'{point = Pub}, Type};
+ed_to_pub(#'ECPrivateKey'{parameters = Type, publicKey = Pub}) ->
+    {#'ECPoint'{point = Pub}, Type}.
+-endif.
+
+-spec encode(public_key:public_key()) -> binary().
+-spec decode(binary()) -> public_key:public_key().
+-if(?OTP_RELEASE < 24).
 encode(Key) ->
-    case erlang:system_info(otp_release) < "24" of
-        true ->
-            public_key:ssh_encode(Key, ssh2_pubkey);
-        false ->
-            ssh_file:encode(Key, ssh2_pubkey)
-    end.
+    public_key:ssh_encode(Key, ssh2_pubkey).
 
 decode(Key) ->
-    case erlang:system_info(otp_release) < "24" of
-        true ->
-            public_key:ssh_decode(Key, ssh2_pubkey);
-        false ->
-            ssh_file:decode(Key, ssh2_pubkey)
-    end.
+    public_key:ssh_decode(Key, ssh2_pubkey).
+-else.
+encode(Key) ->
+    ssh_file:encode(Key, ssh2_pubkey).
+
+decode(Key) ->
+    ssh_file:decode(Key, ssh2_pubkey).
+-endif.
 
 split(<<D:70/binary, Rest/binary>>) ->
     [D, $\n | split(Rest)];
